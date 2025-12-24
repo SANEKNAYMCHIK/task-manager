8b33e67be1d53bc0f52175fc76603d89d097b41e
SANEKNAYMCHIK <sashanaumov053@yandex.ru>
2025-12-24 18:20:16 +0300

    Added methods of service task

--- internal/services/task_service.go
+++ internal/services/task_service.go
@@ -9,3 +9,23 @@ type TaskService struct {
 func NewTaskService(data *sync.Map) *TaskService {
 	return &TaskService{data: data}
 }
+
+func (s *TaskService) Create() {
+	return
+}
+
+func (s *TaskService) Update() {
+	return
+}
+
+func (s *TaskService) Delete() {
+	return
+}
+
+func (s *TaskService) Get() {
+	return
+}
+
+func (s *TaskService) List() {
+	return
+}

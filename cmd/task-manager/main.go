c355fd1a3822f21c6e85d29039decb92192086c3
SANEKNAYMCHIK <sashanaumov053@yandex.ru>
2025-12-29 00:04:23 +0300

    Added graceful shutdown

--- cmd/task-manager/main.go
+++ cmd/task-manager/main.go
@@ -1,21 +1,50 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"sync"
+	"syscall"
+	"time"
 
 	"github.com/SANEKNAYMCHIK/task-manager/internal/handlers"
 	"github.com/SANEKNAYMCHIK/task-manager/internal/services"
 )
 
 func main() {
-	serverPort := "8080"
+	serverPort := ":8080"
 	data := &sync.Map{}
 	taskService := services.NewTaskService(data)
 	mux := handlers.NewRouter(taskService)
-	if err := http.ListenAndServe(":"+serverPort, mux); err != nil {
-		fmt.Printf("Server error: %v\n", err)
+	httpServer := &http.Server{
+		Addr:    serverPort,
+		Handler: mux,
 	}
 
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM, syscall.SIGINT, syscall.SIGTERM)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		if err := httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Server error: %v\n", err)
+		}
+	}()
+	<-stop
+	log.Println("Shutting down server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := httpServer.Shutdown(ctx); err != nil {
+		log.Printf("Server shutdown error: %v", err)
+	}
+	wg.Wait()
+	log.Println("Server stopped")
 }
